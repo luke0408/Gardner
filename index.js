45b7ee2a88dd4b288b3791749a0a42b7edee8609
zjvl324 <zjvl324@gmail.com>
2025-07-29 14:09:49 +0000

    feat: test logger

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+const logger = (req, res, next) => {
+  console.log('LOGGED');
+  next();
+};
+
+app.use(logger);
+
 app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
