4d9bad0a89177fd88080873687bd5f9e559de059
zjvl324 <zjvl324@gmail.com>
2025-08-04 14:06:11 +0000

    refactor: dto code change to ts

--- apps/server/src/dtos/temp.response.dto.js
+++
@@ -1,7 +0,0 @@
-export const tempResponseDTO = (data) => {
-  return { testString: data };
-};
-
-export const flagResponseDTO = (flag) => {
-  return { flag: flag };
-};
---
+++ apps/server/src/dtos/temp.response.dto.ts
@@ -0,0 +1,7 @@
+export const tempResponseDTO = (data: Data) => {
+  return { testString: data };
+};
+
+export const flagResponseDTO = (flag: number) => {
+  return { flag: flag };
+};
