73d8efb5f540cf4743e805fb48f831415b6217ca
zjvl324 <zjvl324@gmail.com>
2025-08-04 13:59:11 +0000

    refactor: move ./config to ./src/config

--- apps/server/src/controllers/github.controller.js
+++ apps/server/src/controllers/github.controller.js
@@ -1,6 +1,6 @@
-import { status } from '../../config/response.status';
-import { getUserContributions } from '../services/github.service';
-import { response } from '../../config/response';
+import { status } from "../config/response.status";
+import { getUserContributions } from "../services/github.service";
+import { response } from "../config/response";
 
 export const userContributions = (req, res, next) => {
   res.send(response(status.SUCCESS, getUserContributions(req.params.username)));
