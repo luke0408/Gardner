f867b0eff49a6a8be243351d7edd506f00d4d7f0
zjvl324 <zjvl324@gmail.com>
2025-08-05 10:34:14 +0000

    refactor: cjs to js

---
+++ apps/server/eslint.config.js
@@ -0,0 +1,39 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import deprecationPlugin from "eslint-plugin-deprecation";
+
+export default [
+  tsPlugin.configs.recommended,
+
+  {
+    files: ["src/**/*.ts", "test/**/*.ts"],
+
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        project: ["tsconfig.json", "test/tsconfig.json"],
+      },
+    },
+
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      depercation: deprecationPlugin,
+    },
+
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
+];
