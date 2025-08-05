27e1c42316ba59b0a2ae435d445bd628bec4d7e9
zjvl324 <zjvl324@gmail.com>
2025-08-05 11:10:51 +0000

    refactor: converte to config.js file

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  parser: "typscript",
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  quoteProps: "as-needed"
+}
