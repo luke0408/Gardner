642d21163251f7f18cc92e61382330cb76e28af5
zjvl324 <zjvl324@gmail.com>
2025-08-05 10:57:13 +0000

    feat: using path method

--- apps/server/build/env.ts
+++ apps/server/build/env.ts
@@ -1,4 +1,12 @@
 import fs from "fs";
+import path from "path";
 
-if (fs.existsSync(`${__dirname}/../.env`) === false)
-  fs.copyFileSync(`${__dirname}/../.env.local`, `${__dirname}/../.env`);
+const envPath = path.resolve(__dirname, "..", ".env");
+const envLocalPath = path.resolve(__dirname, "..", ".env.local");
+
+if (!fs.existsSync(envPath)) {
+  if (!fs.existsSync(envLocalPath))
+    throw new Error("Missing .env.local – cannot seed default .env");
+
+  fs.copyFileSync(envLocalPath, envPath, fs.constants.COPYFILE_EXCL);
+}
