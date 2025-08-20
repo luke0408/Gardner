ce8dcb8ee76a054ee8fdcce337d1c299159af224
zjvl324 <zjvl324@gmail.com>
2025-08-20 09:27:53 +0000

    feat: add  response form

--- apps/server/src/types/index.ts
+++ apps/server/src/types/index.ts
@@ -4,3 +4,44 @@ import * as githubQueries from "./graphql/github.queries";
 export type Merge<F, S> = {
   [K in keyof (F & S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
 };
+
+interface ResponseForm<T> {
+  result: true;
+  code: 1000;
+  requestToResponse?: `${number}ms`;
+  data: T;  
+}
+
+interface ErrorForm { 
+  result: false; 
+  code: number; 
+  data: string;
+};
+
+interface PaginationForm<T extends PaginationType.InitialPaginationResponseType> {
+  result: true;
+  code: 1000;
+  requestToResponse?: `${number}ms`;
+  data: PaginationType.PaginationResponseType<T>;
+}
+
+namespace PaginationType {
+  export interface InitialPaginationResponseType {
+    list: any[];
+    count: number;
+  }
+  
+  export interface PaginationResponseType<T extends InitialPaginationResponseType> {
+    list: T["list"];
+    count: T["count"];
+    totalResult: number;
+    totalPage: number;
+    search?: string;
+    page: number;
+  }
+}
+
+export type Try<T> = ResponseForm<T>;
+export type TryCatch<T, E extends ErrorForm> = ResponseForm<T> | E;
+export type TryPagination<T extends PaginationType.InitialPaginationResponseType> = PaginationForm<T>;
+export type TryCatchPagination<T extends PaginationType.InitialPaginationResponseType, E extends ErrorForm> = PaginationForm<T> | E;
