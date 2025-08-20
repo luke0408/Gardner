6d135610a91502c827b65100d9512e59516dde8c
zjvl324 <zjvl324@gmail.com>
2025-08-20 05:35:49 +0000

    feat: add throttler

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,12 +1,20 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common"
+import { APP_GUARD } from "@nestjs/core";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { CacheModule } from "@nestjs/cache-manager";
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot ({throttlers: [{ttl: 60, limit: 10}]}),
     CacheModule.register({ isGlobal: true })
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    }
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
