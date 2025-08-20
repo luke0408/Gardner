11b5535b2e473950bf048ecdf0764664e8874d2d
zjvl324 <zjvl324@gmail.com>
2025-08-20 06:34:05 +0000

    feat: merge type

--- apps/server/src/types/index.ts
+++ apps/server/src/types/index.ts
@@ -1,3 +1,6 @@
 import * as graphOperation from "./graphql/operations";
 import * as githubQueries from "./graphql/github.queries";
 
+export type Merge<F, S> = {
+  [K in keyof (F & S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
+};
