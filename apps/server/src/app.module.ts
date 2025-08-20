6f111130b8c190b4e4a0ead5f8d4f169d8651f63
zjvl324 <zjvl324@gmail.com>
2025-08-20 14:45:24 +0000

    feat: update module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,11 +4,19 @@ import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { CacheModule } from "@nestjs/cache-manager";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver } from "@nestjs/apollo";
+import { GithubModule } from "./external/github/github.module";
 
 @Module({
   imports: [
     ThrottlerModule.forRoot ({throttlers: [{ttl: 60, limit: 10}]}),
-    CacheModule.register({ isGlobal: true })
+    CacheModule.register({ isGlobal: true }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    GithubModule,
   ],
   controllers: [],
   providers: [
