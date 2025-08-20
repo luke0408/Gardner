9d2654e1ac91f5d4ff392e06ed81b4ba50a29e76
zjvl324 <zjvl324@gmail.com>
2025-08-20 05:37:06 +0000

    feat: http exection fileter to module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,7 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common"
-import { APP_GUARD } from "@nestjs/core";
+import { APP_FILTER, APP_GUARD } from "@nestjs/core";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { CacheModule } from "@nestjs/cache-manager";
+import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 
 @Module({
   imports: [
@@ -13,7 +14,11 @@ import { CacheModule } from "@nestjs/cache-manager";
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
-    }
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
   ],
 })
 export class AppModule implements NestModule {
