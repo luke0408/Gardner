f721a544b983aeaff458d57a09edf9f44a2138bf
zjvl324 <zjvl324@gmail.com>
2025-08-21 11:02:48 +0000

    feat: add interfactor provider on main module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,5 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common"
-import { APP_FILTER, APP_GUARD } from "@nestjs/core";
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { CacheModule } from "@nestjs/cache-manager";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
@@ -7,6 +7,8 @@ import { LoggerMiddleware } from "./common/middlewares/logger.middleware";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver } from "@nestjs/apollo";
 import { GithubModule } from "./external/github/github.module";
+import { TimeoutInterceptor } from "./common/interceptors/timeout.interceptor";
+import { LoggingInterceptor } from "./common/interceptors/logging.interceptor";
 
 @Module({
   imports: [
@@ -27,6 +29,14 @@ import { GithubModule } from "./external/github/github.module";
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
     },
   ],
 })
