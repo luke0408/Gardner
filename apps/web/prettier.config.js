49dcdb6a28afcc645fad8ce5ec76d0436e85952c
zjvl324 <zjvl324@gmail.com>
2025-08-23 09:32:34 +0000

    feat: import config

--- apps/web/prettier.config.js
+++ apps/web/prettier.config.js
@@ -1,5 +1,14 @@
 import baseConfig from "../../prettier.config.js";
+import pluginSortImports from "@ianvs/prettier-plugin-sort-imports";
 
 export default {
   ...baseConfig,
+  plugins: [pluginSortImports],
+  importOrder: [
+    "^react",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/",
+    "^[./]",
+  ],
 };
